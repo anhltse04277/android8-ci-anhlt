0b8a0d6f652648fc02e763e87f4521d74218a809
DESKTOP-LPSQP1J\AnhLe <anhltse04277@fpt.edu.vn>
2017-02-20 21:21:48 +0700

    Đây là bài tập về nhà

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,12 +1,18 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.*;
+import java.io.*;
 
 /**
  * Created by AnhLe on 2/19/2017.
  */
 public class GameWindow extends Frame {
+    Image backgroundImage;
+    Image planeImage;
+    int planeX = 180;
+    int planeY =  550;
+
+
     public GameWindow(){
         setVisible(true);
         setSize(400,600);
@@ -23,5 +29,77 @@ public class GameWindow extends Frame {
                 System.out.println("WindowClosed");
             }
         });
+        // 1: load image
+        try {
+            backgroundImage = loadImages("background.png");
+            planeImage = loadImages("plane3.png");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // dung repaint(); cung on
+        repaint();
+       // update(getGraphics());
+        // 2: draw image
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch(e.getKeyCode()){
+                    case KeyEvent.VK_RIGHT:{
+                        if(planeX +10 <= 360 ){
+                            planeX += 10;
+                            repaint();
+                        }
+                        break;
+                    }
+                    case KeyEvent.VK_LEFT:{
+                        if(planeX-10 >= 0){
+                            planeX -=10;
+                            repaint();
+                        }
+                        break;
+                    }
+                    case KeyEvent.VK_UP:{
+                        if(planeY -10 >= 0 ){
+                            planeY -= 10;
+                            repaint();
+                        }
+                        break;
+                    }
+                    case KeyEvent.VK_DOWN:{
+                        if(planeY +10 <= 550 ){
+                            planeY += 10;
+                            repaint();
+                        }
+                        break;
+                    }
+                }
+
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+            }
+        });
+    }
+    private Image loadImages(String url ){
+        try {
+            Image image = ImageIO.read(new File("resourses/" + url));
+            return image;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void update(Graphics g) {
+        g.drawImage(backgroundImage,0,0,400,600,null);
+        g.drawImage(planeImage,planeX,planeY,40,50,null);
+        // x = (400-w)  /2
+        // y = 600 -25
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Hello  asuka kirara");
+       // System.out.println(" Hello  asuka kirara");
         GameWindow gameWindow = new GameWindow();
     }
 }
